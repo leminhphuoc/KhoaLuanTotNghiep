c62a337424896fdff339521fde4962f93b4813ee
Phuoc .Le <phuoc.le@tekcent.com>
2020-08-22 10:54:04 +0700

    update 22-08

---
+++ Models/Entity/Dictionary.cs
@@ -0,0 +1,20 @@
+﻿namespace Models.Entity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Dictionary")]
+    public partial class Dictionary
+    {
+        public long Id { get; set; }
+
+        [StringLength(250)]
+        public string Key { get; set; }
+
+        [StringLength(250)]
+        public string Value { get; set; }
+    }
+}
